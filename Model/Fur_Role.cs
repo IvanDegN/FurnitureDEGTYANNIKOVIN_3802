7fee3da5143d950c21b16b8fb87963de7b440544
Ivan Degtyannikov <mauzer.bruno@yandex.ru>
2021-04-12 17:54:08 +0300

    Добавьте файлы проекта.

---
+++ Model/Fur_Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FurnitureDEGTYANNIKOVIN_3802
+{
+    partial class Fur_Role
+    {
+        public override string ToString()
+        {
+            return Role;
+        }
+    }
+}
